f00bbfa9a5e271429ea31f5990d77cebfc8b441d
adavies312 <a.m.davies1020@gmail.com>
2018-04-18 17:05:42 -0500

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,55 +13,60 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 
 //Computer Picks a Random Letter
-window.onload = function() {
+window.onload = function () {
 	var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    computerGuess.push(compGuess);
-    //display computer guess
-	console.log(computerGuess[0]);
+	computerGuess.push(compGuess);
+	//display computer guess
+	//console.log(computerGuess[0]);
 }
+
+//only lets user pick a letter of the alphabet
+//if (event.keyCode >=65 && event.keyCode <=90) 
 
 
 //User picks letter
-document.onkeyup = function(event) {
-	var playerGuess = event.key;
-    lettersGuessed.push(playerGuess);
-    //display players guess
-	console.log(playerGuess[0]);
+document.onkeyup = function (event) {
+	var userGuess = event.key;
+	lettersGuessed.push(userGuess);
+	//display players guess
+	//console.log(userGuess[0]);
 
-//if player guesses right and has more than 0 turns left
-if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-    wins++;
-    guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-	computerGuess.push(compGuess);
-    //display new computer letter
-	console.log(computerGuess[0]);
-	winzSpan.textContent = wins;
+	//if player guesses right and has more than 0 turns left...game resets
+	if ((userGuess === computerGuess[0]) && (guessesLeft > 0)) {
+		wins++;
+		alert("You really are a psychic");
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
+		var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+		computerGuess.push(compGuess);
+		//display new computer letter
+		//console.log(computerGuess[0]);
+
+
+	}
+
+	// if player doesn't guess right and still has guesses left...game continues
+	else if ((userGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+		guessesLeft--;
+	}
+
+	//if player has no turns left and guesses wrong...game resets
+	else {
+		losses++;
+		alert("Better luck next time");
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
+		var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+		computerGuess.push(compGuess);
+		//display new computer letter
+		//console.log(computerGuess[0]);
+
+	}
 	
-
-}
-
-// if player doesn't guess right and still has guesses left
-else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-	guessesLeft--;
-}
-
-//if player has no turns left and guesses wrong
-else {
-	losses++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-	computerGuess.push(compGuess);
-	//display new computer letter
-	console.log(computerGuess[0]);
+	winzSpan.textContent = wins;
 	lossezSpan.textContent = losses;
-
-}
-
-leftSpan.innerHTML = guessesLeft;
-lettersSpan.innerHTML = lettersGuessed;
+	leftSpan.textContent = guessesLeft;
+	lettersSpan.textContent = lettersGuessed;
 }
